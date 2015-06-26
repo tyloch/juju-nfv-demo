1c7a9ad1934a51fe3016e8e92e64948679956ddc
Artur Tyloch <artur@at-mba-u.T-mobile.com>
2015-06-25 21:44:07 -0700

    update clean-env.sh with all services

--- clean-env.sh
+++ clean-env.sh
@@ -1,3 +1,10 @@
-juju remove-service clearwater-sprout
+juju remove-service clearwater-sprout -v
 juju remove-service clearwater-bono
 juju remove-service clearwater-ralf
+juju remove-service clearwater-ellis
+juju remove-service clearwater-homer
+juju remove-service clearwater-homestead
+juju remove-service dns
+juju remove-service mysql
+juju remove-service telscale-load-balancer
+juju remove-service telscale-restcomm
